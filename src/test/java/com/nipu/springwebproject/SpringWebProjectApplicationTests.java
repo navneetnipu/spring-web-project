b79c1f4727163ca506b5b279dc4321f7f78feaec
NAVNEET NIPU <40489986+navneetnipu@users.noreply.github.com>
2021-08-04 12:11:13 +0530

    initial

---
+++ src/test/java/com/nipu/springwebproject/SpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipu.springwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
